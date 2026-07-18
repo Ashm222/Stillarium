b2256e9446e84a9675e4f38fed5bb438b1382b28
Ashm222 <ash.252@hotmail.co.uk>
2026-07-18 19:36:09 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "stillarium-v16";
+const CACHE = "stillarium-v17";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
